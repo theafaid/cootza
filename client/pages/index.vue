fb0f9a4667fcca3ad220823a990596c42bf0baa7
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-07-08 19:58:49 +0200

    Initial Vue Categories

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -9,33 +9,44 @@
       <div class="col-md-3 col-xs-6" v-for="(category, index) in categories">
         <div id="accordion">
           <div class="card shadowing">
-            <div
-              :id="'heading-'+index"
-              data-toggle="collapse"
-              :data-target="'#collapse-'+index"
-              aria-expanded="false"
-              :aria-controls="'#collapse-'+index"
-              class="card-header collapsed">
-              <h5 class="mb-0">
-                <button class="btn btn-link">
-                  <i :class="category.icon"></i> {{category.name}}
-                </button>
-              </h5>
+            <div v-if="! category.children.length">
+              <div class="card-header">
+                <h5 class="mb-0">
+                  <nuxt-link :to="{name: 'categories'}" class="btn btn-link" style="" :key="category.slug">
+                    <i :class="category.icon"></i> {{category.name}}
+                  </nuxt-link>
+                </h5>
+              </div>
             </div>
-            <div
-              v-if="category.children.length"
-              :id="'collapse-'+index"
-              :aria-labelledby="'heading-'+index"
-              data-parent="#accordion"
-              class="collapse">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li
-                    v-for="(child, index) in category.children"
-                    class="list-group-item list-group-item-light shadowing">
-                    <a href="/" class="btn btn-link" v-text="child.name"></a>
-                  </li>
-                </ul>
+            <div v-else>
+              <div class="card-header collapsed">
+                <h5 class="mb-0">
+                  <nuxt-link :to="{name: 'categories'}" class="btn btn-link" style="" :key="category.slug">
+                    <i :class="category.icon"></i> {{category.name}}
+                  </nuxt-link>
+                  <i
+                    :id="'heading-'+index"
+                    data-toggle="collapse"
+                    :data-target="'#collapse-'+index"
+                    aria-expanded="false"
+                    :aria-controls="'#collapse-'+index"
+                    class="fe fe-arrow-down float" ></i>
+                </h5>
+              </div>
+              <div
+                :id="'collapse-'+index"
+                :aria-labelledby="'heading-'+index"
+                data-parent="#accordion"
+                class="collapse">
+                <div class="card-body">
+                  <ul class="list-group">
+                    <li
+                      v-for="(child, index) in category.children"
+                      class="list-group-item list-group-item-light shadowing">
+                      <a href="/" class="btn btn-link" v-text="child.name"></a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
