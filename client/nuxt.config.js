da2c14875519baa1776e0a3605477ded5feb9d92
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-07-08 11:14:20 +0200

    Install tabler ui template

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -11,6 +11,23 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
+    style: [
+      {rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'},
+    ],
+    script: [
+      {src: "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+        integrity: "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
+        crossorigin: "anonymous"},
+
+      {src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+        integrity: "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1",
+        crossorigin: "anonymous"},
+
+      {src: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+        integrity: "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM",
+        crossorigin: "anonymous"}
+
+    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
@@ -23,7 +40,9 @@ export default {
   ** Global CSS
   */
   css: [
+    "@/assets/css/dashboard.css"
   ],
+
   /*
   ** Plugins to load before mounting the App
   */
@@ -33,8 +52,7 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
-    // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt',
+
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
   ],
