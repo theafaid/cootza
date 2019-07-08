da28d4c60a299bd9ffc4fa4a31a4f43a7e900753
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-07-08 10:08:46 +0200

    Structure api

--- server/app/Providers/RouteServiceProvider.php
+++ server/app/Providers/RouteServiceProvider.php
@@ -37,7 +37,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
+        // $this->mapWebRoutes();
 
         //
     }
@@ -65,9 +65,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
+        Route::middleware('api')
+            //->prefix('api')
+            //->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
 }
