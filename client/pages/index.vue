30313cb8d25e6314d0443ee1b9e534a2101e41de
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-07-09 12:54:53 +0200

    Edit Index Page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="container">
     <div class="page-header">
-      <h1 class="page-title">
-        Home
-      </h1>
+      <i class="icon-shopping-bag"></i>
+      <div class="col-lg-12 ml-auto">
+        <form class="input-icon my-3 my-lg-0">
+          <input type="search" class="form-control header-search" placeholder="Search&hellip;" tabindex="1">
+          <div class="input-icon-addon">
+            <i class="fe fe-search"></i>
+          </div>
+        </form>
+      </div>
     </div>
     <div class="row row-cards">
       <div class="col-md-3 col-xs-6" v-for="(category, index) in categories">
@@ -23,18 +29,15 @@
             <div v-else>
               <div class="card-header collapsed">
                 <h5 class="mb-0">
-                  <nuxt-link
-                    :to="{name: 'categories-category', params: {category: category.slug}}"
-                    class="btn btn-link" style="" :key="category.slug">
-                    <i :class="category.icon"></i> {{category.name}}
-                  </nuxt-link>
-                  <i
+                  <button
                     :id="'heading-'+index"
                     data-toggle="collapse"
                     :data-target="'#collapse-'+index"
                     aria-expanded="false"
                     :aria-controls="'#collapse-'+index"
-                    class="fe fe-arrow-down float" ></i>
+                    class="btn btn-link" style="" :key="category.slug">
+                    <i :class="category.icon"></i> {{category.name}}
+                  </button>
                 </h5>
               </div>
               <div
@@ -44,6 +47,14 @@
                 class="collapse">
                 <div class="card-body">
                   <ul class="list-group">
+                    <li
+                      class="list-group-item list-group-item-light shadowing">
+                      <nuxt-link
+                        :to="{name: 'categories-category', params: {category: category.slug}}"
+                        class="btn btn-link">
+                        All in {{category.name}}
+                      </nuxt-link>
+                    </li>
                     <li
                       v-for="(child, index) in category.children"
                       class="list-group-item list-group-item-light shadowing">
