0904658c51382913ffd31c3fc6675b6ed9150286
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-07-09 16:12:29 +0200

    Fetch Ads

---
+++ client/pages/ads/_ad/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>
+      {{ad}}
+    </p>
+</template>
+
+<script>
+  export default {
+
+    data(){
+      return {
+        ad: null
+      }
+    },
+
+    async asyncData(context){
+      let response = await context.$axios.$get(`ads/${context.params.ad}`);
+      return {ad: response.data};
+    },
+  }
+</script>
