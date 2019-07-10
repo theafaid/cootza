d378d6947697e7ed74710e50bada66e975d8cf50
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-07-10 16:00:59 +0200

    Testing filter advertisements by category

--- server/tests/Feature/Advertisements/AdvertisementIndexTest.php
+++ server/tests/Feature/Advertisements/AdvertisementIndexTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Advertisements;
 
 use Facades\Tests\Setup\AdvertisementFactory;
+use Facades\Tests\Setup\CategoryFactory;
 use Tests\TestCase;
 
 class AdvertisementIndexTest extends TestCase
@@ -27,5 +28,22 @@ class AdvertisementIndexTest extends TestCase
         $ads->each(function ($ad) use ($response){
             $response->assertJsonFragment(['slug' => $ad->slug]);
         });
+    }
+
+    /** @test */
+    function can_filter_advertisements_by_its_category()
+    {
+        $ads = AdvertisementFactory::create(2);
+
+        $otherAd = AdvertisementFactory::create();
+
+        $response = $this->getJson(route('advertisements.index', ['category' => $ads[0]->category->slug]));
+
+        $response->assertJsonCount(2, 'data');
+
+        $response->assertJsonFragment(
+            ['slug' => $ads[0]->slug],
+            ['slug' => $ads[1]->slug]
+        );
     }
 }
