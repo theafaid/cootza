253cbe3eb07a95be791a805037833b2992ff3832
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-07-10 17:30:04 +0200

    View ads in a specific category

--- client/pages/categories/_category/_subCategory/index.vue
+++ client/pages/categories/_category/_subCategory/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>sub category page</h1>
+  <div>
+    <h6 v-for="ad in ads">
+      <nuxt-link :to="{name: 'ads-ad', params: {ad: ad.slug}}">{{ad.title}}</nuxt-link>
+    </h6>
+  </div>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        ads: []
+      }
+    },
+
+    async asyncData(context){
+      const response = await context.$axios.$get(`/ads?category=${context.params.subCategory}`);
+
+      return {ads: response.data};
+    }
+  }
+</script>
