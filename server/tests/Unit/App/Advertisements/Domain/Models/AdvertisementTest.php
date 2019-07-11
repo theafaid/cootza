e47b0622e241fa13f3f034bb8368f9a381a8104a
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-07-11 23:07:47 +0200

    An advertisement belongs to an owner

--- server/tests/Unit/App/Advertisements/Domain/Models/AdvertisementTest.php
+++ server/tests/Unit/App/Advertisements/Domain/Models/AdvertisementTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Unit\App\Ads\Domain\Models;
 
 use App\App\Categories\Domain\Models\Category;
+use App\Generic\Domain\Models\User;
 use Facades\Tests\Setup\CategoryFactory;
 use Facades\Tests\Setup\AdvertisementFactory;
+use Facades\Tests\Setup\UserFactory;
 use Tests\TestCase;
 
 class AdvertisementTest extends TestCase
@@ -32,5 +34,15 @@ class AdvertisementTest extends TestCase
             $preferredCategoryToSwapWith->id,
             $ad->preferredCategoryToSwapWith->id
         );
+    }
+
+    /** @test */
+    function it_belongs_to_a_user()
+    {
+        $ad = AdvertisementFactory::ownedBy($user = UserFactory::create())->create();
+
+        $this->assertInstanceOf(User::class, $ad->owner);
+
+        $this->assertEquals($user->id, $ad->owner->id);
     }
 }
